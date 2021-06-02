3e8468c07de87236d5a1545238da6c49019b32db
NguyenLongKim <longkimpyxq2@gmail.com>
2021-06-02 20:25:28 +0700

    update minimax.py

--- minimax.py
+++ minimax.py
@@ -3,6 +3,35 @@ from common import *
 class SupportMNM:
     previousBoard = None
 
+def minimax(board, player, depth, maxDepth, alpha, beta, previousBoard):
+    '''
+    Return a tuple (best heuristic value, best movement)
+    '''
+    if depth == maxDepth:
+        return (heuristic(board,player),None)
+    else:
+        movements = generateMovements(board,player,previousBoard)
+        if len(movements) == 0:
+            return (heuristic(board,player),None)
+        bestMovement = None
+        for movement in movements:
+            tmpBoard = copyBoard(board)
+
+            # update board when player execute the move
+            updateBoard(tmpBoard,player,movement)
+
+            # get negative of best heuristic value of opponent after player execute the move
+            newValue = -minimax(tmpBoard,-player,depth+1, maxDepth, -beta, -alpha, board)[0]
+
+            if newValue>alpha:
+                alpha = newValue
+                bestMovement = movement
+
+            # cutoff  
+            if alpha>=beta:
+                return (alpha,bestMovement)
+
+        return (alpha,bestMovement)
 
 def move_minimax(board, player):
     maxDepth = 4
